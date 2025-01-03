483bc6d31c9ba7ff3651df19fe4ddbd8d38153d6
aaron-kz <im.aaronkz@gmail.com>
2025-01-03 14:49:21 -0500

    Cambia la font-family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ['Lato', 'sans-serif'], },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
